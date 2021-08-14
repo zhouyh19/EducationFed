7e54a76a4505ddc6c4f85cab58e73b29dab17546
Seok-Ju Hahn <seokjuhahn@unist.ac.kr>
2021-08-15 01:16:33 +0900

    update models for CIFAR10

--- src/models.py
+++ src/models.py
@@ -33,15 +33,15 @@ class CNN(nn.Module):
         self.name = name
         self.activation = nn.ReLU(True)
 
-        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=hidden_channels, kernel_size=(5, 5), padding=1, stride=1, bias=True)
-        self.conv2 = nn.Conv2d(in_channels=hidden_channels, out_channels=hidden_channels * 2, kernel_size=(5, 5), padding=1, stride=1, bias=True)
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=hidden_channels, kernel_size=(5, 5), padding=1, stride=1, bias=False)
+        self.conv2 = nn.Conv2d(in_channels=hidden_channels, out_channels=hidden_channels * 2, kernel_size=(5, 5), padding=1, stride=1, bias=False)
         
         self.maxpool1 = nn.MaxPool2d(kernel_size=(2, 2), padding=1)
         self.maxpool2 = nn.MaxPool2d(kernel_size=(2, 2), padding=1)
         self.flatten = nn.Flatten()
 
-        self.fc1 = nn.Linear(in_features=(hidden_channels * 2) * (7 * 7), out_features=num_hiddens, bias=True)
-        self.fc2 = nn.Linear(in_features=num_hiddens, out_features=num_classes, bias=True)
+        self.fc1 = nn.Linear(in_features=(hidden_channels * 2) * (7 * 7), out_features=num_hiddens, bias=False)
+        self.fc2 = nn.Linear(in_features=num_hiddens, out_features=num_classes, bias=False)
 
     def forward(self, x):
         x = self.activation(self.conv1(x))
@@ -55,3 +55,32 @@ class CNN(nn.Module):
         x = self.fc2(x)
         return x
 
+# for CIFAR10
+class CNN2(nn.Module):
+    def __init__(self, name, in_channels, hidden_channels, num_hiddens, num_classes):
+        super(CNN2, self).__init__()
+        self.name = name
+        self.activation = nn.ReLU(True)
+
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=hidden_channels, kernel_size=(5, 5), padding=1, stride=1, bias=False)
+        self.conv2 = nn.Conv2d(in_channels=hidden_channels, out_channels=hidden_channels * 2, kernel_size=(5, 5), padding=1, stride=1, bias=False)
+        
+        self.maxpool1 = nn.MaxPool2d(kernel_size=(2, 2), padding=1)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=(2, 2), padding=1)
+        self.flatten = nn.Flatten()
+
+        self.fc1 = nn.Linear(in_features=(hidden_channels * 2) * (8 * 8), out_features=num_hiddens, bias=False)
+        self.fc2 = nn.Linear(in_features=num_hiddens, out_features=num_classes, bias=False)
+
+    def forward(self, x):
+        x = self.activation(self.conv1(x))
+        x = self.maxpool1(x)
+
+        x = self.activation(self.conv2(x))
+        x = self.maxpool2(x)
+        x = self.flatten(x)
+    
+        x = self.activation(self.fc1(x))
+        x = self.fc2(x)
+        
+        return x
